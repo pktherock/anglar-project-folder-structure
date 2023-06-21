f2b5f5d999a2b8e9f3557600c0ff5ac8a05226f2
pktherock <pktherock00@gmail.com>
2023-06-22 01:01:15 +0530

    gurad and routing not working properly

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HistoryComponent } from './components/history/history.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageNotFoundComponent,
+    HistoryComponent
+  ],
   imports: [
     CommonModule
   ]
